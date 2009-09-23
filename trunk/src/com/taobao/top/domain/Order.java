9e3b86d0ac02e4c1b8d600dff28477a605128254
carver.gu <carver.gu@a425f8a2-956b-11de-b6d4-8762f465f3f9>
2009-09-23 15:22:26 +0000

    refine code

--- trunk/src/com/taobao/top/domain/Order.java
+++ trunk/src/com/taobao/top/domain/Order.java
@@ -1,5 +1,6 @@
 package com.taobao.top.domain;
 
+import com.taobao.top.mapping.JsonClass;
 import com.taobao.top.mapping.JsonProperty;
 
 /**
@@ -8,6 +9,7 @@ import com.taobao.top.mapping.JsonProperty;
  * @author carver.gu
  * @since 1.0, Sep 17, 2009
  */
+@JsonClass("orders")
 public class Order extends BaseObject {
 
 	/** 商品编号 */
