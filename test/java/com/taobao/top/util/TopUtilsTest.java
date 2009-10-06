729a968826b0f65ad4fed61c543ba001f922a553
carver.gu <carver.gu@a425f8a2-956b-11de-b6d4-8762f465f3f9>
2009-10-06 15:15:03 +0000

    support top call back parameters decode

--- test/java/com/taobao/top/util/TopUtilsTest.java
+++ test/java/com/taobao/top/util/TopUtilsTest.java
@@ -1,5 +1,6 @@
 package com.taobao.top.util;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,14 +8,14 @@ import junit.framework.Assert;
 
 import org.junit.Test;
 
-public class SysUtilsTest {
+public class TopUtilsTest {
 
 	@Test
 	public void signTopRequest() throws Exception {
 		Map<String, String> params = new HashMap<String, String>();
 		params.put("nick", "winwindg");
 		params.put("fields", "nick,sex");
-		String sign = SysUtils.signTopRequest(params, "carver");
+		String sign = TopUtils.signTopRequest(params, "carver");
 		Assert.assertEquals("E780A69E04E438846FAF25AD28047415", sign);
 	}
 
@@ -23,8 +24,15 @@ public class SysUtilsTest {
 		Map<String, String> params = new HashMap<String, String>();
 		params.put("nick", "风胜");
 		params.put("fields", "nick,sex");
-		String sign = SysUtils.signTopRequest(params, "carver");
+		String sign = TopUtils.signTopRequest(params, "carver");
 		Assert.assertEquals("C94566A40E066217E83292751B225F2C", sign);
+	}
+
+	@Test
+	public void decodeTopParams() throws IOException {
+		String topParams = "aWZyYW1lPTEmdHM9MTI1NDgzMjI5NDU1MiZ2aWV3X21vZGU9ZnVsbCZ2aWV3X3dpZHRoPTAmdmlzaXRvcl9pZD0yMjY3MDQ1OCZ2aXNpdG9yX25pY2s9aHowNzk5";
+		Map<String, String> map = TopUtils.decodeTopParams(topParams);
+		Assert.assertEquals("hz0799", map.get("visitor_nick"));
 	}
 
 }
