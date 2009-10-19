b9a6d513731af0863443c8ae9aecf3a198716a0e
carver.gu <carver.gu@a425f8a2-956b-11de-b6d4-8762f465f3f9>
2009-10-19 11:22:36 +0000

    remove getParameters method

--- trunk/src/com/taobao/top/util/TopContext.java
+++ trunk/src/com/taobao/top/util/TopContext.java
@@ -13,18 +13,38 @@ public class TopContext {
 
 	private Map<String, String> parameters = new HashMap<String, String>();
 
+	/**
+	 * 获取应用编号。
+	 * 
+	 * @return 应用编号
+	 */
 	public String getAppKey() {
 		return getParameter("top_appkey");
 	}
 
+	/**
+	 * 获取授权码。
+	 * 
+	 * @return 授权码
+	 */
 	public String getSessionKey() {
 		return getParameter("top_session");
 	}
 
+	/**
+	 * 获取回调签名。
+	 * 
+	 * @return 回调签名
+	 */
 	public String getSignature() {
 		return getParameter("top_sign");
 	}
 
+	/**
+	 * 获取淘宝用户编号。
+	 * 
+	 * @return 淘宝用户编号
+	 */
 	public Long getUserId() {
 		String userId = getParameter("visitor_id");
 		if (StrUtils.isEmpty(userId)) {
@@ -34,19 +54,32 @@ public class TopContext {
 		}
 	}
 
+	/**
+	 * 获取淘宝用户昵称。
+	 * 
+	 * @return 淘宝用户昵称
+	 */
 	public String getUserNick() {
 		return getParameter("visitor_nick");
 	}
 
+	/**
+	 * 获取指定参数的值。
+	 * 
+	 * @param key 参数名
+	 * @return 参数值
+	 */
 	public String getParameter(String key) {
 		return this.parameters.get(key);
 	}
 
-	public void addParameters(Map<String, String> parameters) {
-		this.parameters.putAll(parameters);
+	protected void addParameters(Map<String, String> parameters) {
+		if (parameters != null && !parameters.isEmpty()) {
+			this.parameters.putAll(parameters);
+		}
 	}
 
-	public void addParameter(String key, String value) {
+	protected void addParameter(String key, String value) {
 		this.parameters.put(key, value);
 	}
 
