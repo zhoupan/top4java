e9d4294745c144dd4cef3ec83713e76f7a1bf40d
carver.gu <carver.gu@a425f8a2-956b-11de-b6d4-8762f465f3f9>
2009-11-07 10:45:33 +0000

    customize json-tree

---
+++ test/java/org/stringtree/json/JSONWriterTest.java
@@ -0,0 +1,26 @@
+package org.stringtree.json;
+
+import java.util.Date;
+
+import org.junit.Test;
+import org.stringtree.json.JSONWriter;
+
+import com.taobao.top.domain.Location;
+import com.taobao.top.domain.User;
+
+public class JSONWriterTest {
+
+	@Test
+	public void writeUser() {
+		User user = new User();
+		user.setNick("风胜");
+		user.setCreated(new Date());
+		Location location = new Location();
+		location.setState("浙江");
+		location.setCity("杭州");
+		user.setLocation(location);
+		JSONWriter writer = new JSONWriter();
+		System.out.println(writer.write(user));
+	}
+
+}
