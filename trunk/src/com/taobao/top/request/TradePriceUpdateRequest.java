8341783c807fd94a333e601f1bb8c879eee35ba0
carver.gu <carver.gu@a425f8a2-956b-11de-b6d4-8762f465f3f9>
2009-11-15 10:18:22 +0000

    support trade price update request

---
+++ trunk/src/com/taobao/top/request/TradePriceUpdateRequest.java
@@ -0,0 +1,43 @@
+package com.taobao.top.request;
+
+import java.util.Map;
+
+import com.taobao.top.util.TopHashMap;
+
+/**
+ * TOP API: taobao.trade.price.update.page
+ * 
+ * @author carver.gu
+ * @since 1.0, Nov 15, 2009
+ */
+public class TradePriceUpdateRequest implements TopRequest {
+
+	private Long tid;
+	private Integer type;
+	private String callbackUrl;
+
+	public void setTid(Long tid) {
+		this.tid = tid;
+	}
+
+	public void setType(Integer type) {
+		this.type = type;
+	}
+
+	public void setCallbackUrl(String callbackUrl) {
+		this.callbackUrl = callbackUrl;
+	}
+
+	public String getApiName() {
+		return "taobao.trade.price.update.page";
+	}
+
+	public Map<String, String> getTextParams() {
+		TopHashMap params = new TopHashMap();
+		params.put("biz_order_id", tid);
+		params.put("biz_type", type);
+		params.put("callback_url", callbackUrl);
+		return params;
+	}
+
+}
