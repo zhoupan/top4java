14aa22080d11421264a5537ef595bd6d508db82b
carver.gu <carver.gu@a425f8a2-956b-11de-b6d4-8762f465f3f9>
2010-04-12 07:51:11 +0000

    dev top4java 2.0

--- test/java/com/taobao/top/request/TradeApiTest.java
+++ test/java/com/taobao/top/request/TradeApiTest.java
@@ -4,9 +4,11 @@ import junit.framework.Assert;
 
 import org.junit.Test;
 
+import com.taobao.top.TopClient;
 import com.taobao.top.TopJsonRestClient;
 import com.taobao.top.domain.PageList;
 import com.taobao.top.domain.Trade;
+import com.taobao.top.parser.json.ListJsonParser;
 import com.taobao.top.util.TestUtils;
 
 /**
@@ -20,12 +22,22 @@ public class TradeApiTest {
 	private TopJsonRestClient client = TestUtils.getOnlineJsonClient();
 
 	@Test
-	public void getTrades() {
+	public void getSoldTrades() {
 		TradesSoldGetRequest req = new TradesSoldGetRequest();
 		req.setFields("tid,seller_nick,buyer_nick,orders.oid,orders.iid,orders.title,orders.price");
 		req.setPageSize(2);
 		PageList<Trade> rsp = client.tradesSoldGet(req);
 		Assert.assertEquals(2, rsp.getReturnResults());
+	}
+
+	@Test
+	public void getBoughtTrades() {
+		TopClient topClient = TestUtils.getPrivateClient();
+		TradesBoughtGetRequest req = new TradesBoughtGetRequest();
+		req.setFields("tid,seller_nick,buyer_nick,orders.oid,orders.iid,orders.title,orders.price");
+		TopRequestProxy proxy = new TopRequestProxy(req, "tbtest1063");
+		PageList<Trade> rsp = topClient.execute(proxy, new ListJsonParser<Trade>(Trade.class));
+		Assert.assertTrue(rsp.getTotalResults() > 0L);
 	}
 
 }
