d98a3c9132b2228c2a8c031b43a34533f80f3eac
carver.gu <carver.gu@a425f8a2-956b-11de-b6d4-8762f465f3f9>
2010-04-12 03:26:25 +0000

    dev top4java 2.0

--- trunk/src/com/taobao/top/TopException.java
+++ trunk/src/com/taobao/top/TopException.java
@@ -10,10 +10,8 @@ public class TopException extends RuntimeException {
 
 	private static final long serialVersionUID = -238091758285157331L;
 
-	private int errCode;
+	private String errCode;
 	private String errMsg;
-	private String subErrCode;
-	private String subErrMsg;
 
 	public TopException() {
 		super();
@@ -31,34 +29,18 @@ public class TopException extends RuntimeException {
 		super(cause);
 	}
 
-	public TopException(int errCode, String errMsg) {
+	public TopException(String errCode, String errMsg) {
 		super(errCode + ":" + errMsg);
 		this.errCode = errCode;
 		this.errMsg = errMsg;
 	}
 
-	public TopException(int errCode, String errMsg, String subErrCode, String subErrMsg) {
-		super(errCode + ":" + errMsg + ", " + subErrCode + ":" + subErrMsg);
-		this.errCode = errCode;
-		this.errMsg = errMsg;
-		this.subErrCode = subErrCode;
-		this.subErrMsg = subErrMsg;
-	}
-
-	public int getErrCode() {
+	public String getErrCode() {
 		return this.errCode;
 	}
 
 	public String getErrMsg() {
 		return this.errMsg;
-	}
-
-	public String getSubErrCode() {
-		return this.subErrCode;
-	}
-
-	public String getSubErrMsg() {
-		return this.subErrMsg;
 	}
 
 }
