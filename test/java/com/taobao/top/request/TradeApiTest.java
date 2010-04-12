cad89a09be915395ce1a66b676be31eeb7f1f613
carver.gu <carver.gu@a425f8a2-956b-11de-b6d4-8762f465f3f9>
2010-04-12 07:04:51 +0000

    dev top4java 2.0

--- test/java/com/taobao/top/request/TradeApiTest.java
+++ test/java/com/taobao/top/request/TradeApiTest.java
@@ -1,8 +1,12 @@
 package com.taobao.top.request;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 
 import com.taobao.top.TopJsonRestClient;
+import com.taobao.top.domain.PageList;
+import com.taobao.top.domain.Trade;
 import com.taobao.top.util.TestUtils;
 
 /**
@@ -13,14 +17,15 @@ import com.taobao.top.util.TestUtils;
  */
 public class TradeApiTest {
 
+	private TopJsonRestClient client = TestUtils.getOnlineJsonClient();
+
 	@Test
 	public void getTrades() {
 		TradesSoldGetRequest req = new TradesSoldGetRequest();
 		req.setFields("tid,seller_nick,buyer_nick,orders.oid,orders.iid,orders.title,orders.price");
 		req.setPageSize(2);
-		TopJsonRestClient client = TestUtils.getOnlineJsonClient();
-		String rsp = client.getResponse(req);
-		System.out.println(rsp);
+		PageList<Trade> rsp = client.tradesSoldGet(req);
+		Assert.assertEquals(2, rsp.getReturnResults());
 	}
 
 }
