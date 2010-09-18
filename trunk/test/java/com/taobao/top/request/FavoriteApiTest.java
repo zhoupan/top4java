01c0d859a5ab2e26ba2cf265eae8fa488b9817c4
carver.gu <carver.gu@a425f8a2-956b-11de-b6d4-8762f465f3f9>
2010-09-18 06:37:58 +0000

    dev...

---
+++ trunk/test/java/com/taobao/top/request/FavoriteApiTest.java
@@ -0,0 +1,34 @@
+package com.taobao.top.request;
+
+import org.junit.Test;
+
+import com.taobao.top.TopJsonRestClient;
+import com.taobao.top.domain.CollectItem;
+import com.taobao.top.domain.PageList;
+import com.taobao.top.util.TestUtils;
+
+/**
+ * 收藏夹接口测试。
+ * 
+ * @author carver.gu
+ * @since 1.0, Sep 18, 2010
+ */
+public class FavoriteApiTest {
+
+	private TopJsonRestClient client = TestUtils.getOnlineJsonClient();
+
+	@Test
+	public void searchFavorite() {
+		FavoriteSearchRequest req = new FavoriteSearchRequest();
+		req.setUserNick("我的小天天宝贝");
+		req.setCollectType("SHOP");
+		req.setPageNo(1);
+		PageList<CollectItem> cis = client.favoriteSearch(req);
+		if (!cis.isEmpty()) {
+			for (CollectItem ci : cis.getContent()) {
+				System.out.println(ci.getItemNumid());
+			}
+		}
+	}
+
+}
